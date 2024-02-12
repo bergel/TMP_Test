efa8f52e28ff25d9415fd67bd305658c5c66a78d
Alexandre Bergel <alexandre.bergel@me.com>
2024-02-12 10:41:20 +0100

    Create run_static_lint.sh

---
+++ run_static_lint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# CLONING AND BUILDING StaticLint.jl
+git clone git@github.com:bergel/StaticLint.jl.git
+cd StaticLint.jl
+echo "HERE: $PWD"
+julia --proj -e "import Pkg ; Pkg.Registry.update() ; Pkg.instantiate() ; Pkg.build()"
+cd ..
+
+# RUNNING THE CHECK
+julia --project=StaticLint.jl -e "using StaticLint ; open(\"result.txt\") do io StaticLint.run_lint(\"src\"; io) end" 
+cat result.txt
