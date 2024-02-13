d808b7c5420bd143a4cd14a90065c9cdf3f5d8b9
Alexandre Bergel <alexandre.bergel@me.com>
2024-02-13 11:03:12 +0100

    Update run_static_lint.sh

--- run_static_lint.sh
+++ run_static_lint.sh
@@ -9,7 +9,7 @@ julia --proj -e "import Pkg ; Pkg.Registry.update() ; Pkg.instantiate() ; Pkg.bu
 cd ..
 
 # RUNNING THE CHECK
-julia --project=StaticLint.jl -e "using StaticLint ; open(\"result.txt\", \"w\") do io StaticLint.run_lint(\"src\"; io) end" 
+julia --project=StaticLint.jl -e "using StaticLint ; open(\"result.txt\", \"w\") do io StaticLint.run_lint(\"src\"; io, filters=essential_filters, formatter=MarkdownFormat()) end" 
 echo "HERE ARE THE RESULTS:"
 cat result.txt
 echo "END OF RESULTS"
