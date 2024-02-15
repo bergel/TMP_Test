61e351be58463498df05c54d0aa4fd59bc575eea
Alexandre Bergel <alexandre.bergel@me.com>
2024-02-15 09:18:28 +0100

    No more error

--- src/test.jl
+++ src/test.jl
@@ -8,7 +8,7 @@ pkg = isempty(ARGS) ? "RAICode" : ARGS[1]
 
 Pkg.activate(@__DIR__)
 
-const n = Threads.nthreads()
+const n = 123 #Threads.nthreads()
 
 function foo(x)
     println("hello")
@@ -16,7 +16,7 @@ end
 
 function run_pkg_build(pkg)
     try
-        @async 1 + 2
+        @spawn 1 + 2
         # x = false
         # true || x
         println(Threads.nthreads())
